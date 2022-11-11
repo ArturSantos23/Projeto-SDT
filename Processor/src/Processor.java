0eac0fdec75384a57c8ca3cd13876cee5f602288
Artur Santos <artur.correia.santos2002@gmail.com>
2022-11-11 11:55:18 +0000

    Sprint 3

--- Processor/src/Processor.java
+++ Processor/src/Processor.java
@@ -10,7 +10,7 @@ public class Processor {
     public static void main(String[] args) {
         try {
             System.setProperty("java.rmi.server.hostname","127.0.0.1");
-            r = LocateRegistry.createRegistry(2023);
+            r = LocateRegistry.createRegistry(2021);
         } catch (RemoteException a) {
             a.printStackTrace();
         }
