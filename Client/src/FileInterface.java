5d3dd943f7e57f21af6f51fb9ea2fc79873e8939
Artur Santos <artur.correia.santos2002@gmail.com>
2022-11-13 21:27:08 +0000

    Sprint 3

--- Client/src/FileInterface.java
+++ Client/src/FileInterface.java
@@ -6,4 +6,5 @@ public interface FileInterface extends Remote{
     void base64ToFile(FileData f) throws IOException;
     String addFile(FileData f) throws RemoteException;
     FileData GetFile(String UIDD) throws IOException;
+    void FileOutput(String RequestID, FileData f) throws RemoteException;
 }
