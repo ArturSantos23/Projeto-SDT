a10f36d81741e34194593a86167313cc295b2b71
Bidarra <aguialpabp@gmail.com>
2022-11-22 19:36:58 +0000

    Sprint 3 - Final

---
+++ Teste.bat
@@ -0,0 +1,15 @@
+@echo OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET "destdir=C:\Users\aguia\Desktop\EI\3A1S\SDT\Projeto-SDT\Storage\src\savedFiles"
+SET "filename1=%destdir%\%1"
+SET "outfile=%destdir%\outfile_%1"
+SET /a count=0
+FOR /f "delims=" %%a IN (%filename1%) DO (
+SET /a count+=1
+SET "line[!count!]=%%a"
+)
+(
+FOR /L %%a IN (%count%,-1,1) DO ECHO(!line[%%a]!
+)>"%outfile%"
+
+GOTO :EOF
